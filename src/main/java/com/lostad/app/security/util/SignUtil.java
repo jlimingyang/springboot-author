09fb73a076ca5b261f11b577153f47a398902988
在水一方发盐人 <sszvip@qq.com>
2017-06-20 15:24:51 +0800

    no commit message

--- src/main/java/com/lostad/app/security/util/SignUtil.java
+++ src/main/java/com/lostad/app/security/util/SignUtil.java
@@ -1,5 +1,7 @@
 package com.lostad.app.security.util;
 
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
@@ -8,6 +10,9 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
 
 /**
  * 
@@ -22,21 +27,24 @@ public class SignUtil {
      * @param 
      * @return String
      */
-	public static String signMD5(String srcStr,String appId,String secretKey) {
+	public static String signMD5(String srcStr,String appsecret) {
 		String dist = null ;//签名
-		String ds = appId+srcStr+secretKey;
+		String ds = "appsecret"+srcStr+appsecret;
 		//System.out.println("---------------ds未加密"+ds);
-		dist = MD5Utils.md5Hex(ds);
+		try {
+			dist = DigestUtils.md5Hex(ds.getBytes("UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 		//System.out.println("- ----ds加密"+dist.toUpperCase());
 		return dist.toUpperCase();//签名是大写的
 	}
-
 	
 	//sign对业务数据的签名 
-	public static boolean verifySignMD5(String src,String appId,String secretKey,String sign){
+	public static boolean verifySignMD5(String src, String appKey,String sign){
 		boolean isOk = false;
 		try{
-			String signNew = signMD5(src,appId,secretKey);
+			String signNew = signMD5(src,appKey);
 		if(signNew.equals(sign)){
 			isOk = true;
 		}
@@ -57,7 +65,6 @@ public class SignUtil {
 	 * @return String
 	 */
 	public static String getParam4Sign(HttpServletRequest request) {
-		String pId = request.getParameter("patientId");
 		List<String> paramList = new ArrayList<String>();
 		//方式一：getParameterMap()，获得请求参数map  
         Map<String,String[]> map= request.getParameterMap();  
@@ -102,68 +109,5 @@ public class SignUtil {
 		}
 		return sb.toString();
 	}
-
-	/**
-	 *方法的说明
-	 * @param 
-	 * @return boolean
-	 */
-	public static boolean verifySignRSA(String json, String pubKey, String sign) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
-	
-	/**
-     * 第三方调用，验证签名
-     *方法的说明
-     * @param 
-     * @return boolean
-     */
-	private boolean isRSASignOk(HttpServletRequest request) throws Exception{
-		boolean isOk = false ;//默认不能通过验证
-//		String appId = request.getHeader("appId");
-//		String sign = request.getHeader("sign");//
-//		try{
-//			String json = IOUtils.toString(request.getInputStream(),"utf-8");
-//			HisThirdParty p = cacheService.getHisThirdParty(Long.parseLong(appId));
-//			if(p==null){
-//				String ip = IpUtil.getIpAddr(request);
-//				throw new ApiException("请联系平台维护人员配置您的公钥！IP:"+ip);
-//			}
-//			json = IOUtils.toString(request.getInputStream(),"utf-8");
-//			if(Validator.isNotEmpty(json)){
-//				if(json.startsWith("\"") && json.endsWith("\"")){
-//					json = json.substring(1, json.length()-1);
-//				}
-//				byte[] data = json.getBytes("utf-8");
-//				byte[] arrSign = sign.getBytes("utf-8"); 
-//				byte[] pub = p.getPubKey().getBytes("utf-8");
-//				
-//				isOk = RsaUtil.verify(data,arrSign, pub);
-//				if(!isOk){
-//					String ip = IpUtil.getIpAddr(request);
-//					logger.error("签名验证失败!ip:"+ip);
-//					throw new ApiException("签名验证失败！");
-//				}
-//				
-//			}else{
-//				String ip = IpUtil.getIpAddr(request);
-//				logger.error("XXXXXXXXXXXXXXX----> data is null，can not validate signature IP:"+ip);
-//				throw new ApiException("未接收到任何json数据！请使用post提交json数据！");
-//			
-//			}
-//			//验证签名
-//			
-//		}catch(Exception e){
-//			e.printStackTrace();
-//			String ip = IpUtil.getIpAddr(request);
-//			logger.error(ip+"================\n"+e.getMessage());
-//			throw new ApiException(e.getMessage());
-//		}
-		
-		return isOk;
-	}
-	
 
 }
