f88cfaba215c4789afc6f67ced714c02998e27f0
在水一方发盐人 <sszvip@qq.com>
2017-07-24 15:44:21 +0800

    no commit message

--- src/main/java/com/lostad/app/common/controller/CommonErrorController.java
+++ src/main/java/com/lostad/app/common/controller/CommonErrorController.java
@@ -1,9 +1,21 @@
 package com.lostad.app.common.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.assertj.core.internal.Strings;
 import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/**
+ * 
+ * @author songsz
+ *
+ */
 @Controller
 public class CommonErrorController implements ErrorController {
 
@@ -14,6 +26,7 @@ public class CommonErrorController implements ErrorController {
 		return "common/error";
 	}
 
+	
 	@Override
 	public String getErrorPath() {
 		return ERROR_PATH;
