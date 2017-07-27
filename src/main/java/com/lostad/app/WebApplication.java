6098d3350df5f65c89bef93587f0ba0cb66b8b83
在水一方发盐人 <sszvip@qq.com>
2017-07-27 17:12:18 +0800

    no commit message

--- src/main/java/com/lostad/app/WebApplication.java
+++ src/main/java/com/lostad/app/WebApplication.java
@@ -1,5 +1,7 @@
 package com.lostad.app;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.lostad.app.common.dao.impl.JpaDaoImpl;
 import com.lostad.app.system.entity.Area;
+import com.lostad.app.system.entity.Role;
 
 @SpringBootApplication
 public class WebApplication implements CommandLineRunner {
@@ -19,13 +22,21 @@ public class WebApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
-//		taskRepository.save(new Task("Task1","Description about task 1", new Date(), false));
-//		taskRepository.save(new Task("Task2","Description about task 2", new Date(), false));
-//		taskRepository.save(new Task("Task3","Description about task 3", new Date(), false));
-//		Area a = new Area();
-//		a.setCode("test");
-//		a.setCreateBy("test");
-//		
-//		jpa.save(a);
+	
+		List list = jpa.findByProperty(Role.class, "roleCode", "administrator");
+		System.out.println("========="+list);
+//		if(list.size()==0){
+//			Role r= new Role("超级管理员","administrator", "all","1");
+//			jpa.save(r);
+//		}
+//		list = jpa.findByProperty(User.class, "username", "sa");
+//		if(list.size()==0){
+//			User r= new User();
+//			r.setUsername("sa");
+//			r.setPassword(MD5Utils.md5Hex("111111"));
+//			r.setRealName("系统管理员");
+//			r.setDelFlag(0);
+//			jpa.save(r);
+//		}
 	}
 }
